9129c0392e434e963f060b52dd4f8188c7b06c15
kimotarek <es-Karim.Ibrahim2025@alexu.edu.eg>
2023-06-26 09:55:20 +0300

    form validate to login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,45 @@ import { users } from '../objects/users';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user_login:any;
+  power=-1;
   constructor(user:users) {
+    this.user_login = user;
    }
 
   ngOnInit(): void {
   }
+
+  power_password(password: string){
+    // Define the regular expressions for each character type
+    console.log(password);
+    const lowercaseRegex = /[a-z]/;
+    const uppercaseRegex = /[A-Z]/;
+    const digitRegex = /\d/;
+    const specialRegex = /[@$!%*?&]/;
+    if(password.length==0){
+         this.power= -1;
+    }
+    // Check if the password meets all criteria
+   else  if (lowercaseRegex.test(password)
+        ||specialRegex.test(password)
+        ||(uppercaseRegex.test(password)
+        && digitRegex.test(password))
+        && password.length >= 11) {
+      this.power= 3;  // strong password
+    }
+    else if (password.length >= 9 
+    || uppercaseRegex.test(password)
+    || digitRegex.test(password)
+    || specialRegex.test(password)
+    )
+    {
+      this.power=2;  // medium password
+    }
+    else if(password.length>=7)   this.power= 1; // weak password
+    else if( password.length!=0)   this.power= 0;
+ 
+  }
+  
+  
 }
