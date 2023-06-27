dbe0ba3808eb73767a1b99cfad407a6edebc981a
kimotarek <es-Karim.Ibrahim2025@alexu.edu.eg>
2023-06-27 03:08:57 +0300

    service protocal

--- src/app/services/servic.service.ts
+++ src/app/services/servic.service.ts
@@ -13,18 +13,23 @@ export class ServicService {
   private apiServerUrl = 'http://localhost:8080';
   constructor(private http: HttpClient) { }
 
-//send user with all date ,recieve flag ==0 if the email or phone are found in system
-//else i recieve flag==1  back active verification code and i will send to backend the verify 
+//send user with all date ,recieve json="success=false" if the email or phone are found in system
+//else i recieve json="success=true"  back active verification code and i will send to backend the verify 
 public sign_up(new_user:any):Observable<any>{
-  return this.http.post<any>(`${this.apiServerUrl}/sign_up`,new_user);
+  return this.http.post<any>(`${this.apiServerUrl}/sign_up`,{email: new_user.email,phone: new_user.phone});
 }
-//send user with email and pass if the user not found recive flag==0 else recive some date i will tell you soon ...
+//send user with email and pass if the user not found recive json="success=false"else recive json="success=true ,user:user" with all data
 public login(user:any):Observable<any>{
   return this.http.post<any>(`${this.apiServerUrl}/login`,user);
 }
-// i will send verify code to back and if it wrong i will recive flag==0 else flag ==1;
-public verify_code(code:any):Observable<any>{
-  return this.http.post<any>(`${this.apiServerUrl}/verify_code`,code);
+// i will send verify code to back and if it code is wrong i will recive json="success=false ", code good but email is token {success=true created=false } redirect to sign up
+// {success=true created=false } redirect to login
+public verify_code(code:any,user:any):Observable<any>{
+  return this.http.post<any>(`${this.apiServerUrl}/verify_code`,{code:code,user:user});
+}
+
+public send_agin(user:any):Observable<any>{
+  return this.http.post<any>(`${this.apiServerUrl}/send_agin`,user.phone);
 }
 // i will send this request if user go to sign up and i recive pair of array ,
 //one to all city ,other to all country. it can be not used and replace this request
