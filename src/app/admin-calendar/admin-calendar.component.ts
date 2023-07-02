3c4274f1406c63296b380cf22928c87944a7084e
kimotarek <es-Karim.Ibrahim2025@alexu.edu.eg>
2023-07-02 12:32:26 +0300

    admin front

--- src/app/admin-calendar/admin-calendar.component.ts
+++ src/app/admin-calendar/admin-calendar.component.ts
@@ -7,9 +7,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminCalendarComponent implements OnInit {
 
+
+  flag_student=false;
+  flag_all_student=true;
+  flag_calender=false;
+  token_exam:any[]=[
+    {
+      title:"Algoritms ",
+      _id:"",
+      country:"Egypt",
+      city:"Alex",
+      location:"sss",
+      snack:"kitkat",
+      day:"15 April",
+      appointment:"3.00 pm",
+      percentage:"00"
+    },
+    {
+      title:"data structure ",
+      _id:"",
+      country:"Egypt",
+      city:"Alex",
+      location:"sss",
+      snack:"kitkat",
+      day:"15 April",
+      appointment:"3.00 pm",
+      percentage:"00"
+    },
+    {
+      title:"data structure ",
+      _id:"",
+      country:"Egypt",
+      city:"Alex",
+      location:"sss",
+      snack:"kitkat",
+      day:"15 April",
+      appointment:"3.00 pm",
+      percentage:"00"
+    },
+    {
+      title:"data structure ",
+      _id:"",
+      country:"Egypt",
+      city:"Alex",
+      location:"sss",
+      snack:"kitkat",
+      day:"15 April",
+      appointment:"3.00 pm",
+      percentage:"00"
+    },
+  ]
+
+
+  current_user = {
+    first_name: "Samy",
+    last_name: "tarek",
+    photo:"../../assets/images/img5.svg",  
+    _id: "1234567"
+  };
+  
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  close_card(){
+    this.flag_student=false;
+  
+  }
 }
