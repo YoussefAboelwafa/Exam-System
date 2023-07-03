ef544f55322739e67148ccd0328cf534333da5f3
kimotarek <es-Karim.Ibrahim2025@alexu.edu.eg>
2023-07-03 15:01:12 +0300

    book user

--- src/app/exams-bar/exams-bar.component.ts
+++ src/app/exams-bar/exams-bar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ServicService } from '../services/servic.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { address } from '../objects/loction_address';
+import { calendar } from '../objects/calender';
 declare const $: any;
 
 @Component({
@@ -40,25 +41,33 @@ export class ExamsBarComponent implements OnInit {
   }
 
   //take it fom back
-  snacks=["kikat",'twix','snickers'];
-  cities=["alex","cairo"];
-  locations=["smouha","sidi gaber"];
-  countries=["egypt","dubai"];
-  days=["14 April","12 May"];
-  Appointments=["3pm","4pm","8pm"]
+  snacks=[];
+  snac:any;
+  locations=[];
+  countries=[];
+  day=[];
+  time=[]
   all_locations: any;
-  country: any;
-  calendar: any;
-  address: any;
+  countrys: any;
+  calendar:calendar[]=[];
+  address: address[]=[];
   temp_city_address:any =null;
-  city: any;
+  citys: any;
   index_city:any;
   index_country: any;
   temp_location_address:any= null;
-  temp_country_address= null;
+  temp_country_address:any= null;
   index_location: any;
   id_location: any;
-  
+  select_snacks:any="Snack";
+  selectedlocation:any="location";
+  selectedCity:any="city";
+  selectedCountry:any="country";
+  selectedday:any='Select a day';
+  selectedappointment:any="Select an Appointmen"
+  index_day:any;
+
+  avilable_time:any;
 
   constructor(private service:ServicService) {
     this.upcoming_exam=this.service.upcoming_ex;
@@ -70,8 +79,9 @@ export class ExamsBarComponent implements OnInit {
     this.service.get_places().subscribe(
       (x)=> {
         this.all_locations=x;
-        this.country= x.map((cont:any)=> cont.country_name);
-
+        console.log(14)
+        this.countrys= x.map((cont:any)=> cont.country_name);
+      
         let combinations: string[] = [];
 
 this.all_locations.forEach((country: any) => {
@@ -86,6 +96,7 @@ this.all_locations.forEach((country: any) => {
 this.service.get_calender().subscribe(
   x=>{
     this.calendar=x;
+
 })
 
 for(let i=0; i<combinations.length; i++) {
@@ -133,44 +144,41 @@ console.log(combinations);
   }
 
   reset_order_exam(){
-    this.order_exam={
-      country:"country",
-      city:"city",
-      location:"location",
-      snack:"snack",
-      day:"Select a day",
-      appointment:"Select an Appointment",
-      _id:"", //id of exam
-      title:"",
-     }
-   }
+ this.select_snacks="Snack";
+ this.selectedlocation="location";
+ this.selectedCity="city";
+ this.selectedCountry="country";
+ this.selectedday='Select a day';
+ this.selectedappointment="Select an Appointmen"
+ this.avilable_time="";
+ }
 
   clear_flag_book(){
   this.flag_snack=false;
   this.flag_book=false;
   this.flag_time=false;
   }
-  select_snack(value:any){
-    this.order_exam.snack=value;
+  // select_snack(value:any){
+  //   this.order_exam.snack=value;
 
-  }
-  select_day(value:any){
-    this.order_exam.day=value;
-  }
-  select_appointment(value:any){
-    this.order_exam.appointment=value;
-  }
+  // }
+  // select_day(value:any){
+  //   this.order_exam.day=value;
+  // }
+  // select_appointment(value:any){
+  //   this.order_exam.appointment=value;
+  // }
 
-  select_country(value:any){
-    this.order_exam.country=value;
+  // select_country(value:any){
+  //   this.order_exam.country=value;
 
-  }
-  select_city(value:any){
-    this.order_exam.city=value;
-  }
-  select_location(value:any){
-    this.order_exam.location=value;
-  }
+  // }
+  // select_city(value:any){
+  //   this.order_exam.city=value;
+  // }
+  // select_location(value:any){
+  //   this.order_exam.location=value;
+  // }
 
   submit_book(){ 
     //service becouse i need city and country and location then next step
@@ -218,56 +226,53 @@ console.log(combinations);
 
 
 
-  onCountrySelected(event: Event,flag:any) {
-    console.log(flag)
-     const selectedCountry = (event.target as HTMLSelectElement).value;
+  onCountrySelected(event: Event) {
+     this.selectedCountry = (event.target as HTMLSelectElement).value;
     // Call your function here with the selectedCountry value
     //flag==1 that meen filter
-    if(flag==1){
     if(this.temp_country_address!=null) {
     this.calendar=this.temp_country_address;
     this.temp_country_address = null;
     }
-    if(selectedCountry != "Country" ){
+    if(this.selectedCountry != "Country" ){
       this.temp_country_address=this.calendar;
-       this.calendar = this.calendar.filter((obj: { country: string; }) => obj.country === selectedCountry);
+       this.calendar = this.calendar.filter((obj: { country: string; }) => obj.country === this.selectedCountry);
 
     }
     else{
-      this.city=[]
+      this.citys=[]
       return
     }
-  }
-    for (let i=0; i<this.country.length; i++){
-      if(this.country[i]==selectedCountry){
+  
+    for (let i=0; i<this.countrys.length; i++){
+      if(this.countrys[i]==this.selectedCountry){
         this.index_country=i;
       }
     }
 
-    this.city= this.all_locations.map((cont:any)=> cont.cities)[this.index_country].map((c:any)=> c.city_name);
+    this.citys= this.all_locations.map((cont:any)=> cont.cities)[this.index_country].map((c:any)=> c.city_name);
   }
 
-  oncitySelected(event: Event,flag:any){
-    const selectedCity = (event.target as HTMLSelectElement).value;
+  oncitySelected(event: Event){
+    this.selectedCity = (event.target as HTMLSelectElement).value;
     // Call your function here with the selectedCountry value
-    if(flag ==1){
     if(this.temp_city_address!=null) {
       this.calendar=this.temp_city_address;
       this.temp_city_address = null;
       }
-    if(selectedCity  != "City" ){
+    if(this.selectedCity  != "City" ){
       
       this.temp_city_address=this.calendar;
-       this.calendar = this.calendar.filter((obj: { city: any; }) => (obj.city === selectedCity) );
+       this.calendar = this.calendar.filter((obj: { city: any; }) => (obj.city === this.selectedCity) );
     }
     else{
       this.locations=[]
       return
     }
-  }
+  
     
-    for (let i=0; i<this.city.length; i++){
-      if(this.city[i]==selectedCity){
+    for (let i=0; i<this.citys.length; i++){
+      if(this.citys[i]==this.selectedCity){
         this.index_city=i;
       }
     }
@@ -275,28 +280,57 @@ console.log(combinations);
     this.locations= this.all_locations.map((cont:any)=> cont.cities)[this.index_country].map((c:any)=> c.locations)[this.index_city].map((c:any)=> c.location_name)
   }
 
-  onlocationSelected(event: Event,flag:any){
-    const selectedlocation = (event.target as HTMLSelectElement).value;
+  onlocationSelected(event: Event){
+    this.selectedlocation = (event.target as HTMLSelectElement).value;
 
-    if(flag==1){
     if(this.temp_location_address!=null) {
       this.calendar=this.temp_location_address;
       this.temp_location_address = null;
       }
-    if(selectedlocation  != "Location" ){
+    if(this.selectedlocation  != "Location" ){
       
       this.temp_location_address=this.calendar;
-       this.calendar = this.calendar.filter((obj: { location: string; }) => (obj.location === selectedlocation) );
+       this.calendar = this.calendar.filter((obj: { location: string; }) => (obj.location === this.selectedlocation) );
     }
-  }
+ 
 
     for (let i=0; i<this.locations.length; i++){
-      if(this.locations[i]==selectedlocation){
+      if(this.locations[i]==this.selectedlocation){
         this.index_location=i;
       }
     }
-    // this.snacks= this.all_locations.map((cont:any)=> cont.cities)[this.index_country].map((c:any)=> c.locations)[this.index_city].map((c:any)=> c.snacks)
+    this.snacks= this.all_locations.map((cont:any)=> cont.cities)[this.index_country].map((c:any)=> c.locations)[this.index_city].map((c:any)=> c.snacks);
     this.id_location= this.all_locations.map((cont:any)=> cont.cities)[this.index_country].map((c:any)=> c.locations)[this.index_city].map((c:any)=> c._id)[this.index_location]
+}
+
+onselectsnack(event: Event){
+  this.select_snacks=(event.target as HTMLSelectElement).value;
+
+  console.log(this.calendar)
+
 
 }
+
+ondayselect(event:Event){
+  this.selectedday=(event.target as HTMLSelectElement).value;
+    console.log(this.selectedday)
+  for(var i=0;i<this.calendar.length;i++){
+    if(this.calendar[i].day_number==this.selectedday.split(" : ")[0]){
+      this.avilable_time=this.calendar[i].time;
+
+  };
+
+  if(this.selectedday=='Select a day'){
+    this.selectedappointment="";
+  }
+}
+}
+
+
+ontimeselect(event:Event){
+  this.selectedappointment=(event.target as HTMLSelectElement).value;
+}
+
+
+
 }
