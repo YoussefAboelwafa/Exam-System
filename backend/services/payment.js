38da6bc2640e79335433a4289e5db7a45a35daef
Hesham-Youssef <hesham_ahmed2002002@yahoo.com>
2023-07-10 21:51:43 +0300

    ABOS EDAK

--- backend/services/payment.js
+++ backend/services/payment.js
@@ -14,12 +14,11 @@ module.exports.start_payment= async (user, exam_info) => {
     try{       
         const {first_name, last_name, email, phone_namber, _id} = user;
         const current_time = Date.now().toString();
-        // const merchantRefNum = JSON.stringify({
-        //     user_id: _id.toString(),
-        //     time: current_time,
-        //     exam_info: exam_info
-        // })
-        const merchantRefNum = '3545345345345345'
+        const merchantRefNum = JSON.stringify({
+            user_id: _id.toString(),
+            time: current_time,
+            exam_info: exam_info
+        })
         const result = await User.updateOne({_id: _id}, {$set: {last_booking_time: current_time}})
         if(result.modifiedCount === 0){
             throw "Error setting last booking time, no money was taken yet"
@@ -41,7 +40,7 @@ module.exports.start_payment= async (user, exam_info) => {
                 }
             ],
             returnUrl: returnUrl,
-            authCaptureModePayment: true,
+            authCaptureModePayment: false,
             signature: sha256(merchant_code + merchantRefNum + "" + returnUrl + exam_info.exam_id + '1' + amount + merchant_hash_key)
         };
         
@@ -54,7 +53,7 @@ module.exports.start_payment= async (user, exam_info) => {
             },
             data: JSON.stringify(data)
         }
-        console.log(data);
+        
         const res = await axios(axiosConfig);
         return res.data
     }catch(err){
