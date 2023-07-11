7c43dc0f79556821c59911c5b1573f7a1a230533
Hesham-Youssef <hesham_ahmed2002002@yahoo.com>
2023-07-11 18:15:28 +0300

    kill me now just do it i don't care anymore

--- backend/services/payment.js
+++ backend/services/payment.js
@@ -3,8 +3,8 @@ const sha256 = require('js-sha256');
 const User = require('../models/User')
 
 const merchant_hash_key = process.env.merchant_hash_key
-const merchant_code = process.env.merchant_code.toString()
-const baseURL = 'https://atfawry.fawrystaging.com/fawrypay-api/api/'
+const merchant_code = process.env.merchant_code
+const baseURL = 'https://atfawry.fawrystaging.com/'
 const amount = '50.00'
 const returnUrl = 'https://youssefaboelwafa.github.io/Exam-System/home/home_bar/' ///check later
 
@@ -12,14 +12,11 @@ const returnUrl = 'https://youssefaboelwafa.github.io/Exam-System/home/home_bar/
 module.exports.start_payment= async (user, exam_info) => {
     try{       
         const {first_name, last_name, email, phone_namber, _id} = user;
-        const current_time = Date.now().toString();
-        const merchantRefNum = JSON.stringify({
-            user_id: _id.toString(),
-            time: current_time,
-            exam_info: exam_info
-        })
-        // const merchantRefNum = "32453543"
+        const current_time = Date.now().toString()
+        const merchantRefNum = _id.toString() + "at" + current_time
         const result = await User.updateOne({_id: _id}, {$set: {last_booking_time: current_time}})
+
+        const unhashed_signature = merchant_code + merchantRefNum +  returnUrl + exam_info.exam_id.toString() + '1' + amount + merchant_hash_key;
         if(result.modifiedCount === 0){
             throw "Error setting last booking time, no money was taken yet"
         }
@@ -29,25 +26,27 @@ module.exports.start_payment= async (user, exam_info) => {
             merchantRefNum: merchantRefNum,
             customerMobile: phone_namber,
             customerEmail: email,
-            customerName: first_name + " " + last_name,
+            customerName: first_name,
             language : "en-gb",
+            
             chargeItems: [
                 {
-                    itemId: exam_info.exam_id, /// check later
+                    itemId: exam_info.exam_id.toString(), /// check later
                     description: 'Product Description', /// change later
                     price: amount, /// change later
-                    quantity: 1
+                    quantity: '1'
                 }
             ],
             returnUrl: returnUrl,
-            authCaptureModePayment: true,
-            signature: sha256(merchant_code + merchantRefNum + "" + returnUrl + exam_info.exam_id + '1' + amount + merchant_hash_key)
+            authCaptureModePayment: false,
+            signature: sha256(unhashed_signature)
         };
+
         
         let axiosConfig = {
             method: 'post',
             baseURL: baseURL, 
-            url: 'payments/init',
+            url: 'fawrypay-api/api/payments/init',
             headers: {
                 'Content-Type': 'application/json'
             },
@@ -55,13 +54,11 @@ module.exports.start_payment= async (user, exam_info) => {
         }
         
         const res = await axios(axiosConfig);
-        console.log(res);
+        // console.log(res);
         return res.data
     }catch(err){
         console.log("hellooo world ---------------------------------------");
-        console.log(err.response);
+        console.log(err);
         return false
     }
 }
-
-
