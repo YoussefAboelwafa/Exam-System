9f093945cad436b5d825611533be51e637f52271
Hesham-Youssef <hesham_ahmed2002002@yahoo.com>
2023-07-12 05:46:47 +0300

    abosss edak asht3al

--- backend/services/payment.js
+++ backend/services/payment.js
@@ -70,7 +70,7 @@ module.exports.get_order = async (merchantRefNumber, old_signature) => {
         const signature = sha256(merchant_code + merchantRefNumber + merchant_hash_key)
 
         let res = await axios.get(`https://atfawry.fawrystaging.com/ECommerceWeb/Fawry/payments/status/v2?merchantCode=${merchant_code}&merchantRefNumber=${merchantRefNumber}&signature=${signature}`)
-        return res
+        return res.data
     } catch (err) {
         console.log("hellooo world ---------------------------------------");
         console.log(err);
