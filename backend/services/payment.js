98156c2162ff53d8c7116d74a2c763e2bfd4ca31
Hesham-Youssef <hesham_ahmed2002002@yahoo.com>
2023-07-12 05:18:15 +0300

    asdf

--- backend/services/payment.js
+++ backend/services/payment.js
@@ -67,40 +67,18 @@ module.exports.start_payment= async (user, exam_info) => {
 
 module.exports.get_order = async (merchantRefNumber, old_signature) => {
     try {
-        // console.log(merchantRefNumber);
-        // const merchant_hash_key_without_dashes = merchant_hash_key.replace(/^-|-$|-/g, '');
-        // const unhashed_signature = merchant_code + merchantRefNumber + merchant_hash_key_without_dashes
-        // console.log(unhashed_signature);
-        // "341c6bad323a486abe2d41c17a4abb2f"
-        // let data = {
-        //     merchantCode: merchant_code,
-        //     merchantRefNumber: merchantRefNumber,
-        //     signature: sha256(unhashed_signature)
-        // };
+        console.log(merchantRefNumber);
+        const unhashed_signature = merchant_code + merchantRefNumber + merchant_hash_key
+        console.log(unhashed_signature);
+        let data = {
+            merchantCode: merchant_code,
+            merchantRefNumber: merchantRefNumber,
+            signature: sha256(unhashed_signature)
+        };
 
-        // let axiosConfig = {
-        //     method: 'GET',
-        //     baseURL: baseURL, 
-        //     url: 'ECommerceWeb/Fawry/payments/status/v2',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify(data)
-        // }
+        console.log(data);
 
-        const jsSHA = require('jssha');
-        let merchantCode = merchant_code;
-        let merchant_sec_key = merchant_hash_key.replace(/^-|-$|-/g, '');
-        let signature_body = merchantCode.concat(merchantRefNumber,merchant_sec_key);
-
-        let sha256 = new jsSHA('SHA-256', 'TEXT');
-        sha256.update(signature_body);
-        let hash_signature = sha256.getHash("HEX");
-        let res = await axios.get('https://atfawry.fawrystaging.com/ECommerceWeb/Fawry/payments/status/v2', {
-                        merchantCode: merchantCode,
-                        merchantRefNumber: merchantRefNumber,
-                        signature: hash_signature
-                                    })
+        let res = await axios.get('https://atfawry.fawrystaging.com/ECommerceWeb/Fawry/payments/status/v2', {data})
         console.log(res);
         return false
     } catch (err) {
