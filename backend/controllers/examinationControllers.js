f0b02d4bbad733f2943cd1e72c6cd8923d410b4c
Hesham Youssef <96203482+Hesham-Youssef@users.noreply.github.com>
2023-07-19 01:10:05 +0300

    finally

--- backend/controllers/examinationControllers.js
+++ backend/controllers/examinationControllers.js
@@ -7,7 +7,7 @@ const payment = require('../services/payment')
 module.exports.get_exam = async (req, res) => {
     try {
         const result = await User.getExam({user_id: req.body.user_id, code:req.body.code})
-        res.json({result: result})
+        res.json(result)
     } catch (error) {
         console.log(error);
         res.json({success: false, error: error})
