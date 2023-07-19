69613356f5e8ccbebb538033f201ad144bda6827
Hesham Youssef <96203482+Hesham-Youssef@users.noreply.github.com>
2023-07-19 20:28:21 +0300

    Update SavedExam.js

--- backend/models/SavedExam.js
+++ backend/models/SavedExam.js
@@ -3,14 +3,14 @@ const Topic = require('./TopicAndQuestion')
 
 const savedExamSchema = new mongoose.Schema({
     exam_id: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'exam'},
-    mcq: {type:{
+    mcq: {type:[{
         question:{type: mongoose.Schema.Types.ObjectId, required: true, ref: 'mcq'},
         user_answer:{type: String, default:''}
-    }},
+    }]},
 
-    coding: {type:{
+    coding: {type:[{
         question:{type: mongoose.Schema.Types.ObjectId, required: true, ref: 'coding'}
-    }}
+    }]}
 });
 
 const SavedExam = mongoose.model('savedExam', savedExamSchema);
