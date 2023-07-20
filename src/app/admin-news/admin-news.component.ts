1bdab3b209111a910d515dbfc2a9238eb197df4f
Youssef Aboelwafa <96186143+YoussefAboelwafa@users.noreply.github.com>
2023-07-20 03:41:14 +0300

    news

--- src/app/admin-news/admin-news.component.ts
+++ src/app/admin-news/admin-news.component.ts
@@ -3,13 +3,44 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-admin-news',
   templateUrl: './admin-news.component.html',
-  styleUrls: ['./admin-news.component.css']
+  styleUrls: ['./admin-news.component.css'],
 })
 export class AdminNewsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  urls: string[] = [
+    '../../assets/images/img4.jpg',
+    '../../assets/images/img6.svg',
+    '../../assets/images/img7.svg',
+  ];
+
+  appendUrl(event: any) {
+    const file = event.target.files[0];
+
+    if (!(file instanceof Blob)) {
+      console.error('Invalid file type');
+      return;
+    }
+
+    // Create a FileReader object
+    const reader = new FileReader();
+
+    // Set up an event listener for when the file is loaded
+    reader.onload = (event: any) => {
+      // Push the data URL to the urls array
+      this.urls.push(event.target.result);
+      console.log(this.urls);
+
+      // Reset the input field
+      event.target.value = '';
+    };
+
+    // Read the file as a data URL
+    reader.readAsDataURL(file);
   }
-
+  deleteNews(index: number) {
+    this.urls.splice(index, 1);
+  }
 }
