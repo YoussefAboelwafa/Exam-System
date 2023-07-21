11d7a4165f8392610c052ff1379b7b501e5af3fa
Youssef Aboelwafa <96186143+YoussefAboelwafa@users.noreply.github.com>
2023-07-22 01:39:34 +0300

    news

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,11 @@ export class AppComponent {
   title = 'Exam-System';
 
   constructor(private router: Router) {
-    // const previousUrl = localStorage.getItem('previousUrl');
-    // if (previousUrl) {
-    //   router.navigateByUrl(previousUrl);
-    //   localStorage.removeItem('previousUrl');
-    // }
-    this.router.navigate(['/admin_home/admin_news']);
+    const previousUrl = localStorage.getItem('previousUrl');
+    if (previousUrl) {
+      router.navigateByUrl(previousUrl);
+      localStorage.removeItem('previousUrl');
+    }
+   
   }
 }
