71a9720efe3b070201113bf4812d1accb226fc19
Youssef Aboelwafa <96186143+YoussefAboelwafa@users.noreply.github.com>
2023-07-22 08:06:06 +0300

    admin news

--- src/app/admin-news/admin-news.component.ts
+++ src/app/admin-news/admin-news.component.ts
@@ -10,50 +10,54 @@ export class AdminNewsComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  manshets: string[] = [
-    'Now you can get your own website ',
-    'C++ advanced course is now available',
-    'New courses are available',
+  News = [
+    {
+      title: 'New courses are available',
+      url: 'https://placehold.co/800x200',
+      blog: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fringilla, libero at mattis scelerisque, dui nisl placerat justo, quis bibendum dui mauris faucibus felis. Nunc egestas fermentum sem, et blandit nibh feugiat vitae. Integer velit neque, laoreet nec nibh at, maximus vehicula nisi. Maecenas tempor nisi id diam finibus auctor. Etiam fermentum semper augue lacinia laoreet. Aliquam imperdiet tincidunt odio in mollis. Cras euismod et massa at eleifend. Phasellus fermentum eu nulla eget cursus',
+    },
+    {
+      title: 'C++ advanced course is now available',
+      url: 'https://placehold.co/800x200',
+      blog: 'This is a blog',
+    },
   ];
 
-  urls: string[] = [
-    '../../assets/images/img6.svg',
-    '../../assets/images/img6.svg',
-    '../../assets/images/img6.svg',
-  ];
 
-  appendUrl(event: any) {
-    const file = event.target.files[0];
+  // appendUrl(event: any) {
+  //   const file = event.target.files[0];
 
-    if (!(file instanceof Blob)) {
-      console.error('Invalid file type');
-      return;
-    }
+  //   if (!(file instanceof Blob)) {
+  //     console.error('Invalid file type');
+  //     return;
+  //   }
 
-    // Create a FileReader object
-    const reader = new FileReader();
+  //   // Create a FileReader object
+  //   const reader = new FileReader();
 
-    // Set up an event listener for when the file is loaded
-    reader.onload = (event: any) => {
-      // Push the data URL to the urls array
-      this.urls.push(event.target.result);
-      console.log(this.urls);
+  //   // Set up an event listener for when the file is loaded
+  //   reader.onload = (event: any) => {
+  //     // Push the data URL to the urls array
+  //     this.urls.push(event.target.result);
+  //     console.log(this.urls);
 
-      // Reset the input field
-      event.target.value = '';
+  //     // Reset the input field
+  //     event.target.value = '';
+  //   };
+
+  //   // Read the file as a data URL
+  //   reader.readAsDataURL(file);
+  // }
+
+  addNews(title: string, url: string, blog: string) {
+    const object = {
+      title: title,
+      url: url,
+      blog: blog,
     };
-
-    // Read the file as a data URL
-    reader.readAsDataURL(file);
-  }
-  deleteUrl(index: number) {
-    this.urls.splice(index, 1);
-  }
-  addNews(value: any) {
-    this.manshets.push(value);
+    this.News.push(object);
   }
   deleteNews(index: number) {
-    this.manshets.splice(index, 1);
+    this.News.splice(index, 1);
   }
-  
 }
